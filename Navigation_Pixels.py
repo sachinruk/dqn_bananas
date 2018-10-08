5473801d21f85544abee44a413f97d19c9085fe9
Sachin Abeywardana <sachin.abeywardana@gmail.com>
2018-10-08 15:15:58 +1100

    pixel training script

---
+++ Navigation_Pixels.py
@@ -0,0 +1,86 @@
+from unityagents import UnityEnvironment
+import numpy as np
+from collections import deque
+import torch
+
+from model import QNetworkCNN, QNetworkDuellingCNN
+from agents import DQNAgent
+
+env = UnityEnvironment(file_name="./Banana.x86_64")
+
+
+# Environments contain **_brains_** which are responsible for deciding the actions of their associated agents. Here we check for the first brain available, and set it as the default brain we will be controlling from Python.
+# get the default brain
+brain_name = env.brain_names[0]
+brain = env.brains[brain_name]
+
+# reset the environment
+env_info = env.reset(train_mode=True)[brain_name]
+
+# number of agents in the environment
+print('Number of agents:', len(env_info.agents))
+
+# number of actions
+action_size = brain.vector_action_space_size
+print('Number of actions:', action_size)
+
+# examine the state space 
+state = env_info.visual_observations[0]
+state_size = state.shape
+print('States have shape:', state.shape)
+
+
+# ### 3. Take Random Actions in the Environment
+# 
+# In the next code cell, you will learn how to use the Python API to control the agent and receive feedback from the environment.
+# 
+# Once this cell is executed, you will watch the agent's performance, if it selects an action (uniformly) at random with each time step.  A window should pop up that allows you to observe the agent, as it moves through the environment.  
+# 
+# Of course, you'll have to change the code so that the agent is able to use its experience to gradually choose better actions when interacting with the environment!
+
+def dqn(agent, n_episodes=2000, max_t=1000, eps_start=1.0, eps_end=0.01, eps_decay=0.995):
+    """Deep Q-Learning.
+    
+    Params
+    ======
+        n_episodes (int): maximum number of training episodes
+        max_t (int): maximum number of timesteps per episode
+        eps_start (float): starting value of epsilon, for epsilon-greedy action selection
+        eps_end (float): minimum value of epsilon
+        eps_decay (float): multiplicative factor (per episode) for decreasing epsilon
+    """
+    scores = []                        # list containing scores from each episode
+    scores_window = deque(maxlen=100)  # last 100 scores
+    eps = eps_start                    # initialize epsilon
+    for i_episode in range(1, n_episodes+1):
+        env_info = env.reset(train_mode=True)[brain_name] # reset the environment
+        state = env_info.visual_observations[0].squeeze().transpose(2,0,1)
+        score = 0
+        for t in range(max_t):
+            action = agent.act(state, eps)
+            env_info = env.step(action)[brain_name]        # send the action to the environment
+            next_state = env_info.visual_observations[0].squeeze().transpose(2,0,1)
+            
+            reward = env_info.rewards[0]                   # get the reward
+            done = env_info.local_done[0]                  # see if episode has finished
+            agent.step(state, action, reward, next_state, done)
+            score += reward                                # update the score
+            state = next_state                             # roll over the state to next time s
+            
+            if done:
+                break 
+        scores_window.append(score)       # save most recent score
+        scores.append(score)              # save most recent score
+        eps = max(eps_end, eps_decay*eps) # decrease epsilon
+        print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)), end="")
+        if i_episode % 100 == 0:
+            print('\rEpisode {}\tAverage Score: {:.2f}'.format(i_episode, np.mean(scores_window)))
+        if np.mean(scores_window)>=13.0:
+            print('\nEnvironment solved in {:d} episodes!\tAverage Score: {:.2f}'.format(i_episode-100, np.mean(scores_window)))
+            torch.save(agent.qnetwork_local.state_dict(), 'checkpointCNN.pth')
+            break
+    return agent, scores
+
+
+agent = DQNAgent(QNetworkDuellingCNN, state_size, action_size, seed=0, ddqn=True)
+agent, scores = dqn(agent, n_episodes=300)
